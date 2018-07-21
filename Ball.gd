01431233590e2d7cb77b707c0dea8d4d163bee42
keith-ferney <keith@ferney.org>
2018-07-20 19:57:52 -0600

    fix assets for android export

--- Ball.gd
+++ Ball.gd
@@ -8,9 +8,14 @@ var gravity = -9.8
 var velocity = Vector3(0,0,0)
 
 func _ready():
+	set_mode(MODE_CHARACTER)
 	pass
 
-func _physics_process(delta):
+func _integrate_forces(state):
+	var camera = get_node("../Camera")
+	# set_rotation_degrees(Vector3(0,camera.get_rotation_degrees().y,0))
+	rotate_y(camera.get_rotation().y - get_rotation().y)
+
 	direction = Vector3(0, 0, 0)
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
@@ -23,13 +28,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_select"):
 		direction.y += 1
 	direction = direction.normalized()
-	direction = direction * speed * delta
+	direction = direction * speed
 	if velocity == null:
 		velocity = Vector3()
-	velocity.y += gravity * delta
+	velocity.y += gravity
 	velocity.x = direction.x
 	velocity.z = direction.z
-	velocity = set_angular_velocity(velocity)
+	velocity = set_linear_velocity(velocity)
+
 
 	if Input.is_action_pressed("ui_jump"):
 		set_axis_velocity(Vector3(0,jumpHeight,0))
---
+++ BallKinematic.gd
@@ -0,0 +1,55 @@
+
+extends KinematicBody
+
+# Member variables
+var g = -9.8
+var vel = Vector3()
+const MAX_SPEED = 50
+const JUMP_SPEED = 7
+const ACCEL= 5
+const DEACCEL= 4
+const MAX_SLOPE_ANGLE = 30
+
+
+func _physics_process(delta):
+	var dir = Vector3() # Where does the player intend to walk to
+	var cam_xform = get_node("../Camera").get_global_transform()
+
+	if Input.is_action_pressed("ui_up"):
+		dir += -cam_xform.basis[2]
+	if Input.is_action_pressed("ui_down"):
+		dir += cam_xform.basis[2]
+	if Input.is_action_pressed("ui_left"):
+		dir += -cam_xform.basis[0]
+	if Input.is_action_pressed("ui_right"):
+		dir += cam_xform.basis[0]
+
+	dir.y = 0
+	dir = dir.normalized()
+
+	vel.y += delta * g
+
+	var hvel = vel
+	hvel.y = 0
+
+	var target = dir * MAX_SPEED
+	var accel
+	if dir.dot(hvel) > 0:
+		accel = ACCEL
+	else:
+		accel = DEACCEL
+
+	hvel = hvel.linear_interpolate(target, accel * delta)
+
+	vel.x = hvel.x
+	vel.z = hvel.z
+
+	vel = move_and_slide(vel, Vector3(0,1,0))
+
+	if is_on_floor() and Input.is_action_pressed("ui_jump"):
+		vel.y = JUMP_SPEED
+
+
+func _on_tcube_body_enter(body):
+	if body == self:
+		get_node("../ty").show()
